b5f102a9900959a9e4de02addf2ad68416024457
Norman Wong <nich19032@outlook.com>
2022-02-07 19:45:12 +0800

    Remove template data

--- docs/intro.md
+++ docs/intro.md
@@ -14,6 +14,7 @@ Get started by **creating a new account** at [pinnacles.app/signup](https://pinn
 - A profile picture (less than 100KB)
 - Your name
 - Your email
+- A password (minimum 6 characters)
 
 ### 2) Tell the IT Team to give you admin access
 - After signing up, the IT team will manually give you admin permissions.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'pinnacles-web-app', // Usually your GitHub org/user name.
   projectName: 'pinnacles-docs', // Usually your repo name.
 
   presets: [
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/pinnacles-web-app/documentation/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/pinnacles-web-app/documentation/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
